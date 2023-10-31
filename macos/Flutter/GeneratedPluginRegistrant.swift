bd5ad1ae7720e6fcef99ea9d7827112a96eeba70
Mohamed Kettouch <mohamed.kettouch@gostudent.org>
2023-10-31 22:58:57 +0100

    bugfix

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,15 +8,19 @@ import Foundation
 import cloud_firestore
 import cloud_functions
 import desktop_webview_auth
+import file_selector_macos
 import firebase_auth
 import firebase_core
 import flutter_secure_storage_macos
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseFunctionsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFunctionsPlugin"))
   DesktopWebviewAuthPlugin.register(with: registry.registrar(forPlugin: "DesktopWebviewAuthPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
---
+++ test.bak/app/src/main/kotlin/medk/bjj/management/bjj_members_management/MainActivity.kt
@@ -0,0 +1,6 @@
+package medk.bjj.management.bjj_members_management
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
